ea899d1262a13346aeb3da00efb6c1b9b42bc2bb
Rucknium <Rucknium@protonmail.com>
2025-06-23 19:46:04 +0000

    Set Shiny app config

---
+++ README.md
@@ -0,0 +1,37 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# `{xmrnetscan}`
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
+
+`{xmrnetscan}` is an alpha project. Expect breaking changes in future
+versions.
+
+## Installation
+
+You can install the development version of `{xmrnetscan}` like so:
+
+Install R. Linux users should install the `r-base` and `r-base-dev`
+system packages.
+
+You can install the development version of `{xmrnetscan}` from
+[GitHub](https://github.com/) if you have `git` and `curl` installed on
+your system with:
+
+``` r
+install.packages("remotes")
+remotes::install_github("Rucknium/xmrnetscan", upgrade = FALSE)
+```
+
+## Run
+
+You can launch the application by running:
+
+``` r
+xmrnetscan::run_app()
+```
--- dev/01_start.R
+++ dev/01_start.R
@@ -20,32 +20,33 @@
 ##
 golem::fill_desc(
   pkg_name = "xmrnetscan", # The name of the golem package containing the app (typically lowercase, no underscore or periods)
-  pkg_title = "PKG_TITLE", # What the Package Does (One Line, Title Case, No Period)
-  pkg_description = "PKG_DESC.", # What the package does (one paragraph).
+  pkg_title = "Shiny App with with Monero Node Data Collected from Network Scans", # What the Package Does (One Line, Title Case, No Period)
+  pkg_description = "Shiny App with with Monero Node Data Collected from Network Scans.", # What the package does (one paragraph).
   authors = person(
-    given = "AUTHOR_FIRST", # Your First Name
-    family = "AUTHOR_LAST", # Your Last Name
-    email = "AUTHOR@MAIL.COM", # Your email
-    role = c("aut", "cre") # Your role (here author/creator)
+    family = "Rucknium", # Your Last Name
+    email = "Rucknium@protonmail.com", # Your email
+    role = c("aut", "cre", "cph"), # Your role (here author/creator),
+    comment = c(ORCID = "0000-0001-5999-8950")
   ),
-  repo_url = NULL, # The URL of the GitHub repo (optional),
+  repo_url = "https://github.com/Rucknium/xmrnetscan", # The URL of the GitHub repo (optional),
   pkg_version = "0.0.0.9000", # The version of the package containing the app
   set_options = TRUE # Set the global golem options
 )
 
 ## Install the required dev dependencies ----
-golem::install_dev_deps()
+# golem::install_dev_deps()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+# usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_agpl_license(version = 3, include_future = TRUE)
 golem::use_readme_rmd(open = FALSE)
 devtools::build_readme()
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+# usethis::use_code_of_conduct(contact = "Golem User")
 usethis::use_lifecycle_badge("Experimental")
-usethis::use_news_md(open = FALSE)
+# usethis::use_news_md(open = FALSE)
 
 ## Init Testing Infrastructure ----
 ## Create a template for tests
@@ -53,7 +54,7 @@ golem::use_recommended_tests()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file.
+# golem::use_favicon() # path = "path/to/ico". Can be an online file.
 # golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
@@ -61,14 +62,14 @@ golem::use_utils_ui(with_test = TRUE)
 golem::use_utils_server(with_test = TRUE)
 
 ## Use git ----
-usethis::use_git()
+# usethis::use_git()
 ## Sets the remote associated with 'name' to 'url'
-usethis::use_git_remote(
-  name = "origin",
-  url = "https://github.com/<OWNER>/<REPO>.git"
-)
+# usethis::use_git_remote(
+#   name = "origin",
+#   url = "https://github.com/<OWNER>/<REPO>.git"
+# )
 
 # You're now set! ----
 
 # go to dev/02_dev.R
-rstudioapi::navigateToFile("dev/02_dev.R")
+# rstudioapi::navigateToFile("dev/02_dev.R")
