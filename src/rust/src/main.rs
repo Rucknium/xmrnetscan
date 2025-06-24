ea028cf7270321282eb4618b33a59112191ea6d3
Rucknium <Rucknium@protonmail.com>
2025-06-24 13:58:10 +0000

    Set Rust to compile stand-alone binary and include it in installed R package

---
+++ src/rust/src/main.rs
@@ -0,0 +1,23 @@
+// This is a comment, and is ignored by the compiler.
+// You can test this code by clicking the "Run" button over there ->
+// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
+// shortcut.
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function.
+
+//! # Monero network crawler
+//!
+//! A simple tool to find all the reachable nodes on the Monero P2P network. It works by recursively connecting to
+//! every peer we are told about in a peer list message, starting by connecting to the seed nodes.
+
+
+
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+}
